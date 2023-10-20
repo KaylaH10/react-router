53016e6356bc3414e701a11e7ad3df7db2362943
Kayla Harrison <kaylaharrison@Kaylas-MacBook-Air.local>
2023-10-19 20:30:07 -0700

    react router workshop - with home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+
+import Blue from "./components/blue";
+import Red from "./components/red";
+import Home from "./components/home";
 
 import "./App.css";
 
@@ -9,11 +13,16 @@ function App() {
   return (
     <>
       <div id="container">
-        <div id="navbar">{/* navigation here */}</div>
+        <div id="navbar">
+          <Link to="/blue">blue</Link>
+          <Link to="/red">red</Link>
+          <Link to="/">home</Link>
+        </div>
         <div id="main-section">
           <Routes>
-            <Route path="/blue" element={<h1>Blue</h1>} />
-            <Route path="/red" element={<h1>Red</h1>} />
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </div>
       </div>
